5b7b8bf5fa7f4703d304f15906682cc667f6ba20
alexey.minay <alexey.minay.dev@gmail.com>
2022-10-07 00:33:11 +0300

    [3] Доработал RawColorsDetector.kt

--- lint-checks/src/main/java/ru/otus/homework/lintchecks/RawColorsDetector.kt
+++ lint-checks/src/main/java/ru/otus/homework/lintchecks/RawColorsDetector.kt
@@ -3,11 +3,19 @@ package ru.otus.homework.lintchecks
 import com.android.resources.ResourceFolderType
 import com.android.tools.lint.detector.api.*
 import org.w3c.dom.Attr
+import org.w3c.dom.Element
 
 @Suppress("UnstableApiUsage")
 class RawColorsDetector : ResourceXmlDetector() {
 
+    private val mColorToNames = mutableMapOf<String, MutableList<String>>()
+    private val mRawColors = mutableListOf<RawColor>()
+
     override fun getApplicableAttributes(): Collection<String>? {
+        return XmlScannerConstants.ALL
+    }
+
+    override fun getApplicableElements(): Collection<String> {
         return XmlScannerConstants.ALL
     }
 
@@ -15,16 +23,73 @@ class RawColorsDetector : ResourceXmlDetector() {
         return folderType in setOf(
             ResourceFolderType.DRAWABLE,
             ResourceFolderType.LAYOUT,
-            ResourceFolderType.MIPMAP
+            ResourceFolderType.MIPMAP,
+            ResourceFolderType.VALUES
         )
     }
 
     override fun visitAttribute(context: XmlContext, attribute: Attr) {
         val value = attribute.value.orEmpty()
         if (value.matches(COLOR_PATTERN)) {
-            context.report(ISSUE, context.getLocation(attribute), BRIEF)
+            mRawColors.add(
+                RawColor(
+                    location = context.getValueLocation(attribute),
+                    color = value.lowercase()
+                )
+            )
         }
     }
+
+    override fun visitElement(context: XmlContext, element: Element) {
+        if (context.file.path.contains("colors.xml")) {
+            val color = element.firstChild.nodeValue.lowercase()
+            if (color.matches(COLOR_PATTERN)) return
+            val name = element.attributes.item(0)?.nodeValue?.lowercase() ?: return
+            when (mColorToNames[color]) {
+                null -> mColorToNames[color] = mutableListOf(name)
+                else -> mColorToNames[color]?.add(name)
+            }
+        }
+    }
+
+    override fun beforeCheckRootProject(context: Context) {
+        mColorToNames.clear()
+        mRawColors.clear()
+    }
+
+    override fun afterCheckRootProject(context: Context) {
+        mRawColors.forEach { rawColor ->
+            val fixValues = mColorToNames[rawColor.color]
+            context.report(
+                ISSUE,
+                rawColor.location,
+                BRIEF,
+                quickfixData = createFix(fixValues, rawColor.location)
+            )
+        }
+    }
+
+    private fun createFix(
+        fixValues: MutableList<String>?,
+        location: Location
+    ): LintFix? {
+        fixValues ?: return null
+
+        val fixes = fixValues.map { value ->
+            fix()
+                .replace()
+                .range(location)
+                .all()
+                .with(value)
+                .build()
+        }
+        return fix().alternatives(*fixes.toTypedArray())
+    }
+
+    data class RawColor(
+        val location: Location,
+        val color: String
+    )
 
     companion object {
         private const val BRIEF = "brief description"
@@ -39,7 +104,10 @@ class RawColorsDetector : ResourceXmlDetector() {
             category = Category.LINT,
             priority = 3,
             severity = Severity.ERROR,
-            implementation = Implementation(RawColorsDetector::class.java, Scope.RESOURCE_FILE_SCOPE)
+            implementation = Implementation(
+                RawColorsDetector::class.java,
+                Scope.RESOURCE_FILE_SCOPE
+            )
         )
 
     }
