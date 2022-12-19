4333cec58782c010f85bb9d7dc927acb2d3df275
m.buev@baibol.ru <3723985d>
2022-12-19 20:57:42 +0300

    Homework Android Lint

--- lint-checks/src/main/java/ru/otus/homework/lintchecks/HomeworkIssueRegistry.kt
+++ lint-checks/src/main/java/ru/otus/homework/lintchecks/HomeworkIssueRegistry.kt
@@ -3,8 +3,9 @@ package ru.otus.homework.lintchecks
 import com.android.tools.lint.client.api.IssueRegistry
 import com.android.tools.lint.detector.api.Issue
 
+@Suppress("UnstableApiUsage")
 class HomeworkIssueRegistry : IssueRegistry() {
 
     override val issues: List<Issue>
-        get() = TODO("Not yet implemented")
+        get() = listOf(GlobalScopeDetector.ISSUE, JobDetector.ISSUE, RawColorsDetector.ISSUE)
 }
---
+++ lint-checks/src/test/kotlin/JobDetectorTest.kt
@@ -0,0 +1,56 @@
+package ru.otus.homework.lintchecks
+
+import com.android.tools.lint.checks.infrastructure.LintDetectorTest.kotlin
+import com.android.tools.lint.checks.infrastructure.TestLintTask.lint
+import org.junit.Test
+
+class JobDetectorTest {
+    class JobInBuilderDetectorTest {
+
+        @Test
+        fun `Check for jobs in coroutine builders`() {
+            lint()
+                .allowMissingSdk()
+                .files(
+                    kotlin(
+                        """
+                        package kotlinx.coroutines
+                        class CoroutineScope { }
+                        class Job { }
+                        class SupervisorJob: Job() {  }
+                        object GlobalScope: CoroutineScope {
+                            fun launch(job: Job, block: () -> Unit) { }
+                        }
+                    """.trimIndent()
+                    ),
+                    kotlin(
+                        """
+                        package tests
+                           
+                        import kotlinx.coroutines.*
+                        class MyViewModel {
+                            init {
+                                GlobalScope.launch(SupervisorJob()) {
+                                    delay(1000)
+                                    println("Hello World")
+                                }
+                            }
+                        }
+                    """.trimIndent()
+                    ),
+
+                    )
+                .issues(JobDetector.ISSUE)
+                .run()
+                .expect(
+                    """
+                    src/tests/MyViewModel.kt:7: Error: Job / Supervisor Job is not allowed [JobInBuilderUsage]
+                            GlobalScope.launch(SupervisorJob()) {
+                                               ~~~~~~~~~~~~~~~
+                    1 errors, 0 warnings
+                """.trimIndent()
+                )
+        }
+
+    }
+}
