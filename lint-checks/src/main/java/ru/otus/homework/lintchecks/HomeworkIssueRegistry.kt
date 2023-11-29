0ef89157c5c92624ad39636a2935607cd16854fe
Dmitrii Lapenko <hwoaran92@mail.ru>
2023-11-30 01:09:39 +0300

    add rules to HomeworkIssueRegistry

--- lint-checks/src/main/java/ru/otus/homework/lintchecks/HomeworkIssueRegistry.kt
+++ lint-checks/src/main/java/ru/otus/homework/lintchecks/HomeworkIssueRegistry.kt
@@ -2,9 +2,16 @@ package ru.otus.homework.lintchecks
 
 import com.android.tools.lint.client.api.IssueRegistry
 import com.android.tools.lint.detector.api.Issue
+import ru.otus.homework.lintchecks.rules.GlobalScopeUsageRule
+import ru.otus.homework.lintchecks.rules.JobInBuilderUsageRule
+import ru.otus.homework.lintchecks.rules.RawColorUsageRule
 
+@Suppress("UnstableApiUsage")
 class HomeworkIssueRegistry : IssueRegistry() {
 
-    override val issues: List<Issue>
-        get() = TODO("Not yet implemented")
+    override val issues: List<Issue> = listOf(
+        GlobalScopeUsageRule.ISSUE,
+        JobInBuilderUsageRule.ISSUE,
+        RawColorUsageRule.ISSUE
+    )
 }
