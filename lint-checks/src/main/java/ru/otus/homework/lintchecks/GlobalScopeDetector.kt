6e65417f0bf2e73c04113d48ed4928d2cc269b96
Kamil Tayupov <k.i.tayupov@mail.ru>
2023-12-06 15:40:46 +0300

    update detector

--- lint-checks/src/main/java/ru/otus/homework/lintchecks/GlobalScopeDetector.kt
+++ lint-checks/src/main/java/ru/otus/homework/lintchecks/GlobalScopeDetector.kt
@@ -1,9 +1,11 @@
 package ru.otus.homework.lintchecks
 
+import com.android.tools.lint.client.api.UElementHandler
 import com.android.tools.lint.detector.api.*
-import com.intellij.psi.PsiMethod
 import org.jetbrains.uast.UCallExpression
-import java.io.File
+import org.jetbrains.uast.UClass
+import org.jetbrains.uast.UElement
+import org.jetbrains.uast.getParentOfType
 
 private const val ID = "GlobalScopeUsage"
 private const val BRIEF_DESCRIPTION = "Не используйте GlobalScope"
@@ -11,7 +13,7 @@ private const val EXPLANATION = """Корутины, запущенные на k
     |скоупа класса, в котором они созданы. Контролировать глобальные корутины неудобно, а отсутствие контроля может 
     |привести к излишнему использованию ресурсов и утечкам памяти."""
 
-class GlobalScopeDetector : Detector(), SourceCodeScanner {
+class GlobalScopeDetector : Detector(), Detector.UastScanner {
 
     companion object {
         val ISSUE = Issue.create(
@@ -28,69 +30,51 @@ class GlobalScopeDetector : Detector(), SourceCodeScanner {
         )
     }
 
-    override fun getApplicableMethodNames(): List<String> {
-        return listOf("launch", "async", "runBlocking", "actor")
+    override fun getApplicableUastTypes(): List<Class<out UElement>> {
+        return listOf(UCallExpression::class.java)
     }
 
-    override fun visitMethodCall(context: JavaContext, node: UCallExpression, method: PsiMethod) {
-        if (node.receiverType?.canonicalText == "kotlinx.coroutines.GlobalScope") {
-            context.report(
-                ISSUE,
-                node,
-                context.getLocation(node),
-                BRIEF_DESCRIPTION,
-                quickFix(context, method)
-            )
-        }
-    }
-
-    private fun quickFix(context: JavaContext, method: PsiMethod): LintFix? {
-        return if (canReplaceWithViewModelScope(context, method)) {
-            fix()
-                .name("Заменить GlobalScope на viewModelScope")
-                .replace()
-                .text("GlobalScope")
-                .with("viewModelScope")
-                .autoFix()
-                .build()
-        } else if (canReplaceWithLifecycleScope(context, method)) {
-            fix()
-                .name("Заменить GlobalScope на lifecycleScope")
-                .replace()
-                .text("GlobalScope")
-                .with("lifecycleScope")
-                .autoFix()
-                .build()
-        } else {
-            null
-        }
-    }
-
-    private fun canReplaceWithViewModelScope(context: JavaContext, method: PsiMethod): Boolean {
-        if (context.evaluator.extendsClass(method.containingClass, "androidx.lifecycle.ViewModel")) {
-            return false
-        }
-        val buildGradleFiles = findBuildGradleFiles(context.project.dir)
-        return buildGradleFiles.any { it.readText().contains("androidx.lifecycle:lifecycle-viewmodel-ktx") }
-    }
-
-    private fun canReplaceWithLifecycleScope(context: JavaContext, method: PsiMethod): Boolean {
-        if (context.evaluator.extendsClass(method.containingClass, "androidx.fragment.app.Fragment")) {
-            return false
-        }
-        val buildGradleFiles = findBuildGradleFiles(context.project.dir)
-        return buildGradleFiles.any { it.readText().contains("androidx.lifecycle:lifecycle-runtime-ktx") }
-    }
-
-    private fun findBuildGradleFiles(directory: File): List<File> {
-        val buildGradleFiles = mutableListOf<File>()
-        directory.listFiles()?.forEach { file ->
-            if (file.isDirectory) {
-                buildGradleFiles.addAll(findBuildGradleFiles(file))
-            } else if (file.name == "build.gradle" || file.name == "build.gradle.kts") {
-                buildGradleFiles.add(file)
+    override fun createUastHandler(context: JavaContext): UElementHandler {
+        return object : UElementHandler() {
+            override fun visitCallExpression(node: UCallExpression) {
+                if (node.receiverType?.canonicalText == "kotlinx.coroutines.GlobalScope") {
+                    context.report(
+                        ISSUE,
+                        node,
+                        context.getLocation(node),
+                        BRIEF_DESCRIPTION,
+                        quickFix(context, node)
+                    )
+                }
             }
         }
-        return buildGradleFiles
+    }
+
+    private fun quickFix(context: JavaContext, node: UCallExpression): LintFix? {
+        return when {
+            canReplaceWithViewModelScope(context, node) -> createReplaceLintFix("viewModelScope")
+            canReplaceWithLifecycleScope(context, node) -> createReplaceLintFix("lifecycleScope")
+            else -> null
+        }
+    }
+
+    private fun createReplaceLintFix(result: String): LintFix {
+        return LintFix.create()
+            .replace()
+            .text("GlobalScope")
+            .with(result)
+            .build()
+    }
+
+    private fun canReplaceWithViewModelScope(context: JavaContext, node: UCallExpression): Boolean {
+        return context.evaluator.extendsClass(node.getParentOfType<UClass>(), "androidx.lifecycle.ViewModel") &&
+                context.evaluator.dependencies?.packageDependencies?.roots
+                    ?.any { it.artifactName == "androidx.lifecycle:lifecycle-viewmodel-ktx" } ?: false
+    }
+
+    private fun canReplaceWithLifecycleScope(context: JavaContext, node: UCallExpression): Boolean {
+        return context.evaluator.extendsClass(node.getParentOfType<UClass>(), "androidx.fragment.app.Fragment") &&
+                context.evaluator.dependencies?.packageDependencies?.roots
+                    ?.any { it.artifactName == "androidx.lifecycle:lifecycle-runtime-ktx" } ?: false
     }
 }
