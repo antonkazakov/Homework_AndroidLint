2cc99ccbfe54bf6e6ea8fe22dde1dc692dd03d52
bmv-2143 <bmv-2143@yandex.ru>
2024-12-03 17:30:59 +0300

    test: add GlobalScopeUsageDetector tests

---
+++ lint-checks/src/test/kotlin/ru/otus/homework/lintchecks/GlobalScopeUsageDetectorTest.kt
@@ -0,0 +1,124 @@
+package ru.otus.homework.lintchecks
+
+import com.android.tools.lint.checks.infrastructure.LintDetectorTest.kotlin
+import com.android.tools.lint.checks.infrastructure.TestLintTask
+import com.android.tools.lint.checks.infrastructure.TestLintTask.lint
+import org.junit.Test
+
+@Suppress("UnstableApiUsage")
+internal class GlobalScopeUsageDetectorTest {
+    private val lintTask: TestLintTask = lint().allowMissingSdk().issues(GlobalScopeUsageDetector.ISSUE)
+
+
+    @Test
+    fun testGlobalScopeCase0() {
+        lintTask
+            .files(
+                kotlin(
+                    """
+                    class GlobalScopeTestCase(private val scope: CoroutineScope) : ViewModel() {
+                    
+                        fun case0() {
+                            GlobalScope.launch {
+                                delay(1000)
+                                println("Hello World")
+                            }
+                        }
+                    }
+               """
+                )
+            )
+            .run()
+            .expect(
+                """src/GlobalScopeTestCase.kt:5: Error: Don't use GlobalScope for coroutines [GlobalScopeUsage]
+                            GlobalScope.launch {
+                            ~~~~~~~~~~~
+1 errors, 0 warnings                 
+            """.trimIndent()
+            )
+    }
+
+    @Test
+    fun testGlobalScopeCase1() {
+        lintTask
+            .files(
+                kotlin(
+                    """
+                    class GlobalScopeTestCase(private val scope: CoroutineScope) : ViewModel() {
+                    
+                            fun case1() {
+                                GlobalScope.launch {
+                                    delay(1000)
+                                    println("Hello World")
+                                }
+                                GlobalScope.actor<String> {
+                                    delay(1000)
+                                    println("Hello World")
+                                }
+                            }
+                    }
+               """
+                )
+            )
+            .run()
+            .expect(
+                """src/GlobalScopeTestCase.kt:5: Error: Don't use GlobalScope for coroutines [GlobalScopeUsage]
+                                GlobalScope.launch {
+                                ~~~~~~~~~~~
+src/GlobalScopeTestCase.kt:9: Error: Don't use GlobalScope for coroutines [GlobalScopeUsage]
+                                GlobalScope.actor<String> {
+                                ~~~~~~~~~~~
+2 errors, 0 warnings""".trimIndent()
+            )
+    }
+
+    @Test
+    fun testGlobalScopeCase2() {
+        lintTask
+            .files(
+                kotlin(
+                    """
+                    class GlobalScopeTestCase(private val scope: CoroutineScope) : ViewModel() {
+                       fun case2() {
+                            viewModelScope.launch {
+                                val deferred = GlobalScope.async {
+                                    delay(1000)
+                                    "Hello World"
+                                }
+                                println(deferred.await())
+                            }
+                        }
+                    }
+               """
+                )
+            )
+            .run()
+            .expect(
+                """src/GlobalScopeTestCase.kt:5: Error: Don't use GlobalScope for coroutines [GlobalScopeUsage]
+                                val deferred = GlobalScope.async {
+                                               ~~~~~~~~~~~
+1 errors, 0 warnings""".trimIndent()
+            )
+    }
+
+    @Test
+    fun testGlobalScopeCase3() {
+        lintTask
+            .files(
+                kotlin(
+                    """
+                    class GlobalScopeTestCase(private val scope: CoroutineScope) : ViewModel() {
+                           fun case3() {
+                                scope.launch {
+                                    delay(1000)
+                                    println("Hello World")
+                                }
+                            }
+                    }
+               """
+                )
+            )
+            .run()
+            .expectClean()
+    }
+}
