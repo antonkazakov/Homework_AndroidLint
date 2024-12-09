d27e77e70eabefacc355f306991b7c96a38c214b
bmv-2143 <bmv-2143@yandex.ru>
2024-12-09 17:40:05 +0300

    refactor: refactor WrongColorUsageDetector, WIP

--- lint-checks/src/main/java/ru/otus/homework/lintchecks/WrongColorUsageDetector.kt
+++ lint-checks/src/main/java/ru/otus/homework/lintchecks/WrongColorUsageDetector.kt
@@ -19,15 +19,19 @@ import java.util.regex.Pattern
 @Suppress("UnstableApiUsage")
 internal class WrongColorUsageDetector : ResourceXmlDetector() {
 
-    private data class ColorToLocation(
+    private data class ColorInfo(
         val location: Location,
-        val color: String
+        val colorValue: String,
+        val context: XmlContext,
+        val element: Element,
+        val attributeName: String,
+        val attributeValue: String
     )
 
-    private val hexColors = mutableListOf<ColorToLocation>()
+    private val hexColorsAndRefs = mutableListOf<ColorInfo>()
 
     // e.g. #FF018786 -> teal_700
-    private val colorValuesToNames = mutableMapOf<String, String>()
+    private val colorValuesToNamesPalette = mutableMapOf<String, String>()
 
     override fun getApplicableAttributes(): Collection<String>? {
         return XmlScannerConstants.ALL
@@ -39,16 +43,21 @@ internal class WrongColorUsageDetector : ResourceXmlDetector() {
 
     override fun visitAttribute(context: XmlContext, attribute: Attr) {
         val attrValue = attribute.value.orEmpty()
-        if (isHexColor(attrValue)) {
-            normalizeColor(attrValue)?.let {
-                hexColors.add(
-                    ColorToLocation(
+        if (isHexColor(attrValue) || isAndroidColorReference(attrValue)) {
+//            normalizeColor(attrValue)?.let {
+                hexColorsAndRefs.add(
+                    ColorInfo(
                         location = context.getValueLocation(attribute),
-                        color = it
+//                        colorValue = it,
+                        colorValue = attrValue,
+                        context = context,
+                        element = attribute.ownerElement,
+                        attributeName = attribute.name,
+                        attributeValue = attrValue
                     )
                 )
-            }
-            println("hexColors: ${hexColors.map { it.color }}")
+//            }
+            println("hexColors: ${hexColorsAndRefs.map { it.colorValue }}")
         }
     }
 
@@ -59,38 +68,174 @@ internal class WrongColorUsageDetector : ResourceXmlDetector() {
     override fun visitElement(context: XmlContext, element: Element) {
         val resourceFolderType = context.resourceFolderType ?: return
 
-        // collect colors from colors.xml
+        // save colors from colors.xml for further reference
         if (resourceFolderType == ResourceFolderType.VALUES &&
             context.file.name == PALETTE_RESOURCE_FILE) {
-            if (collectColorFromPalette(element)) return
+            if (saveColorToMap(element)) return
+
         }
+//        else {
+//            val attributes: NamedNodeMap = element.attributes
+//            for (i in 0 until attributes.length) {
+//                val attr: Node = attributes.item(i)
+//                val attrName = attr.localName ?: continue
+//                val attrValue = attr.nodeValue?.trim() ?: continue
+//
+//                // Проверяем, выглядит ли значение как цвет
+//                // Может быть:
+//                //   1) Hex цвет (#RRGGBB или #AARRGGBB)
+//                //   2) Ссылка на ресурс цвета (@color/...), @android:color/...
+//                //   Ссылка вида @color/teal_700 - это ок, она уже из палитры или нет - проверим потом.
+//                //   Нас интересуют сырые цвета (#FF000000), а также @android:color, т.к. они не в палитре проекта.
+//
+//                // Если @color/... (без android), это скорее всего палитра или другой цвет из проекта
+//                // Если @android:color/... - это системный цвет, который не в палитре.
+//                // Если #... - это сырой цвет
+//                if (isHexColor(attrValue) || isAndroidColorReference(attrValue)) {
+//                    hexColors += ColorToLocation(
+//                        colorValue = attrValue,
+//                        context = context,
+//                        element = element,
+//                        attributeName = attrName,
+//                        attributeValue = attrValue,
+//                        location = context.getValueLocation(attr as Attr)
+//                    )
+//                }
+//            }
+//        }
     }
 
-    private fun collectColorFromPalette(element: Element): Boolean {
+    private fun saveColorToMap(element: Element): Boolean {
         val name = element.getAttribute("name")
         val colorTextValue = element.textContent?.trim() ?: return true
 
         normalizeColor(colorTextValue)?.let { normalized ->
-            colorValuesToNames[normalized.uppercase()] = name
-            println("palette colors: ${normalized.uppercase()} -> $name")
+            colorValuesToNamesPalette[normalized.uppercase()] = name
         }
         return false
     }
 
+    private fun isAndroidColorReference(value: String): Boolean {
+        return value.startsWith("@android:color/")
+    }
+
     override fun afterCheckRootProject(context: Context) {
-        hexColors.forEach { hexColor ->
-            val colorNameToReplace = colorValuesToNames[hexColor.color]
-            val location = hexColor.location
-            val fix = colorNameToReplace?.let {
-                quickColorFix(location = location, newColor = it)
+
+        // ------------------- works ----
+//        hexColors.forEach { hexColor ->
+//            val colorNameToReplace = colorValuesToNamesPalette[hexColor.colorValue]
+//            val location = hexColor.location
+//            val fix = colorNameToReplace?.let {
+//                quickColorFix(location = location, newColor = it)
+//            }
+//            context.report(
+//                issue = ISSUE,
+//                location = location,
+//                message = BRIEF_DESCRIPTION,
+//                quickfixData = fix
+//            )
+//        }
+
+        // -------------------
+
+//        for (info in hexColors) {
+//            val rawValue = info.colorValue
+//
+//            if (rawValue.startsWith("#")) {
+//                val location = info.context.getValueLocation(info.element.getAttributeNode(info.attributeName))
+//                val fix = colorValuesToNamesPalette[rawValue.uppercase()]?.let {
+//                    quickColorFix(location = location, newColor = it)
+//                }
+//                context.report(
+//                    issue = ISSUE,
+//                    location = location,
+//                    message = BRIEF_DESCRIPTION,
+//                    quickfixData = fix
+//                )
+//            }
+//        }
+        // -------------------
+
+        for (info in hexColorsAndRefs) {
+            val rawValue = info.colorValue
+
+            if (rawValue.startsWith("#")) {
+                // Это сырой HEX цвет
+                val normalized = normalizeColor(rawValue)
+                if (normalized != null) {
+                    val colorName = colorValuesToNamesPalette[normalized.uppercase()]
+                    if (colorName != null) {
+                        // Есть совпадение в палитре
+                        // Предлагаем фикс: заменить сырой цвет на @color/colorName
+//                        val fix = LintFix.create()
+//                            .replace()
+//                            .name("Заменить на @color/$colorName")
+//                            .text(rawValue)
+//                            .with("@color/$colorName")
+//                            .autoFix()
+//                            .build()
+
+                        val fix = LintFix.create()
+                            .replace()
+                            .range(info.location)
+//                            .text(rawValue)
+                            .with("")
+                            .build()
+
+                        info.context.report(
+                            issue = WrongColorUsageDetector.ISSUE,
+                            scope = info.element,
+//                            location = info.context.getLocation(info.element.getAttributeNode(info.attributeName)),
+                            location = info.location,
+//                            message = "Цвет $rawValue есть в палитре, используйте @color/$colorName вместо сырого цвета.",
+                            message = BRIEF_DESCRIPTION,
+                            quickfixData = fix
+                        )
+
+                    } else {
+
+                        val fix = LintFix.create()
+                            .replace()
+                            .range(info.location)
+//                            .text(rawValue)
+                            .with("")
+                            .build()
+
+                        // Нет совпадения в палитре
+                        info.context.report(
+                            issue = WrongColorUsageDetector.ISSUE,
+                            scope = info.element,
+//                            location = info.context.getLocation(info.element.getAttributeNode(info.attributeName)),
+                            location = info.location,
+//                            message = "Используется сырой цвет $rawValue, отсутствующий в палитре. Добавьте его в палитру или используйте существующий цвет."
+                            message = BRIEF_DESCRIPTION,
+                            quickfixData = fix
+
+                        )
+                    }
+                }
+            } else if (rawValue.startsWith("@android:color/")) {
+
+                val fix = LintFix.create()
+                    .replace()
+                    .range(info.location)
+//                            .text(rawValue)
+                    .with("")
+                    .build()
+
+                // Системный цвет - не из палитры
+                info.context.report(
+                    issue = WrongColorUsageDetector.ISSUE,
+                    scope = info.element,
+//                    location = info.context.getLocation(info.element.getAttributeNode(info.attributeName)),
+                    location = info.location,
+//                    message = "Используется системный цвет $rawValue, которого нет в палитре. Добавьте соответствующий цвет в палитру или используйте уже имеющийся."
+                    message = BRIEF_DESCRIPTION,
+                    quickfixData = fix
+                )
             }
-            context.report(
-                issue = ISSUE,
-                location = location,
-                message = BRIEF_DESCRIPTION,
-                quickfixData = fix
-            )
         }
+
     }
 
     private fun quickColorFix(location: Location, newColor: String): LintFix {
